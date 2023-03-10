a3b14abbb19abe35b6bef04e0efb4390f887bc54
sunmin <polarlsm1231@gmail.com>
2023-03-10 12:01:59 +0900

    feat: client side hydrated으로 변경

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { hydrate } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+hydrate(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
 );
